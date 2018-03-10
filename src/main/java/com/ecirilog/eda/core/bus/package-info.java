ed48f970a1ca9032c1943ccbbd054fdfc2761bde
Edgar Cirilo <edcirilo_9216@live.com.mx>
2018-03-09 23:46:16 -0600

    Adding core packages

---
+++ src/main/java/com/ecirilog/eda/core/bus/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for {@link com.amk.eda.core.bus.EventBus} interface.
+ *
+ * @author Edgar Cirilo Gonzalez &lt;proed1612@gmail.com&gt;
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+package com.ecirilog.eda.core.bus;
---
+++ src/main/java/com/ecirilog/eda/core/event/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for all representations of the {@link com.amk.eda.core.event.Event}.
+ *
+ * @author Edgar Cirilo Gonzalez &lt;proed1612@gmail.com&gt;
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+package com.ecirilog.eda.core.event;
